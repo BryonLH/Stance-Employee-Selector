15bd8e4d47c68fe19b1cee2667b9930eaede08a0
BryonLH <bryonlh@gmail.com>
2019-07-25 22:44:02 -0700

    updated

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,18 +1,26 @@
-var namesArray = ["Melanie", "Dean", "Daniel", "Stacy", "Amy", "Biggie", "Val"];
+var namesArray = ["filler", "Melanie", "Dean", "Daniel", "Stacy", "Amy", "Biggie", "Val"];
 var employeeNum = 0;
+var employeeIndex = 0;
 var employeesRemoved = [];
-var num = 7;
+var employeesToRemove = [];
 
 
 $("#start-button").on("click", function (e) {
     // e.preventDefault();
     console.log("clicked");
+    while (employeesToRemove.length < 6) {
+        var num = Math.floor(Math.random() * 7) + 1;
+        if ($.inArray(num, employeesToRemove) == -1) {
+            employeesToRemove.push(num);
+            console.log(employeesToRemove);
+        }
+    }
     for (var i = 0; i < namesArray.length; i++) {
         console.log(namesArray[i]);
-        employeeNum++;
         var employeeId = "employee" + employeeNum;
         console.log(employeeId);
         console.log(namesArray.length);
+        employeeNum++;
         $(`<p class='employees ${namesArray[i]}' value='${namesArray[i]}'>${namesArray[i]}</p>`).appendTo("#" + employeeId);
     };
 
@@ -24,18 +32,16 @@ $("#start-button").on("click", function (e) {
     setTimeout(removeEmployee, 3000);
 
     // while (namesArray.length > 1) {
-        
+
     function removeEmployee() {
-        if () {
-            console.log("3 seconds!");
-            var employeeToRemove = Math.floor(Math.random() * num);
-            console.log("the random number is " + employeeToRemove);
-            console.log(employeeToRemove);
-            num--;
-            employeesRemoved.push(employeeToRemove);
-            $("#employee" + employeeToRemove).empty();
-            console.log(employeesRemoved);
-        }
+        console.log("3 seconds!");
+        var employeeToRemove = employeesToRemove[employeeIndex];
+        
+        console.log("the random number is " + employeeToRemove);
+        employeesRemoved.push(employeeToRemove);
+        $("#employee" + employeeToRemove).empty();
+        console.log(employeesRemoved);
+        employeeIndex++;
     }
 
     // function fifteenSeconds() {
@@ -47,35 +53,17 @@ $("#start-button").on("click", function (e) {
     // $("." + employeeToRemove).empty();
     // }
 
-    // function twelveSeconds() {
+    // function removeEmployee() {
     //     console.log("3 seconds!");
-    //     var employeeToRemove = namesArray[Math.floor(Math.random() * 7) + 1];
+    //     var employeeToRemove = Math.floor(Math.random() * num);
+        
+    //     console.log("the random number is " + employeeToRemove);
     //     console.log(employeeToRemove);
+    //     num--;
     //     employeesRemoved.push(employeeToRemove);
-    //     $("#" + employeeToRemove).empty();
+    //     $("#employee" + employeeToRemove).empty();
+    //     console.log(employeesRemoved);
     // }
 
-    // function nineSeconds() {
-    //     console.log("3 seconds!");
-    //     var employeeToRemove = namesArray[Math.floor(Math.random() * 7) + 1];
-    //     console.log(employeeToRemove);
-    //     employeesRemoved.push(employeeToRemove);
-    //     $("#" + employeeToRemove).empty();
-    // }
 
-    // function sixSeconds() {
-    //     console.log("3 seconds!");
-    //     var employeeToRemove = namesArray[Math.floor(Math.random() * 7) + 1];
-    //     console.log(employeeToRemove);
-    //     employeesRemoved.push(employeeToRemove);
-    //     $("#" + employeeToRemove).empty();
-    // }
-
-    // function threeSeconds() {
-    //     console.log("3 seconds!");
-    //     var employeeToRemove = namesArray[Math.floor(Math.random() * 7) + 1];
-    //     console.log(employeeToRemove);
-    //     employeesRemoved.push(employeeToRemove);
-    //     $("#" + employeeToRemove).empty();
-    // }
 })
